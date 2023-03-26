c9b2048f39ad1e02b4d4b38f2b6706e9333f8385
vatiza <rakibur.mac@gmail.com>
2023-03-27 03:24:39 +0600

    country res

--- js/country.js
+++ js/country.js
@@ -4,10 +4,21 @@ const loadCountries = () => {
     .then(data=> displayCountries(data))
 }
 const displayCountries = countries => {
+
     const countriesContainer = document.getElementById('all-countries');
-    // for (const country of countries) {
-    //     console.log(country);
+    // for(const country of countries){
+    //     console.log(country);   
     // }
+    countries.forEach(country =>{
+        console.log(country.capital);
+        const countryDiv=document.createElement('div');
+        countryDiv.classList.add('country');
+        countryDiv.innerHTML=`
+        <h3>Name: ${country.name.common}</h3>
+        <p>Capital: ${country.capital ? country.capital[0]: 'No Capital'}</p>
+        `;
+        countriesContainer.appendChild(countryDiv);
+    })
     
 }
 loadCountries();
