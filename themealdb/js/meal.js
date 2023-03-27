7e4c3785a0e88c902cd000700a6032e587d690e0
vatiza <rakibur.mac@gmail.com>
2023-03-28 02:41:42 +0600

    theMealDB

---
+++ themealdb/js/meal.js
@@ -0,0 +1,35 @@
+const loadMeals=()=>{
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+    .then(res=>res.json())
+    .then(data=>displayMeals(data.meals))
+
+}
+
+const displayMeals=meals=>{
+    // console.log(meals);
+    const mealsContainer=document.getElementById('meals-container');
+    meals.forEach(meal=>{
+        console.log(meal)
+        const mealDiv=document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML=`
+    <div class="card">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+            content. This content is a little bit longer.</p>
+    </div>
+</div>
+        `;
+    mealsContainer.appendChild(mealDiv);
+    })
+}
+const searchMeals=()=>{
+   const searchText=document.getElementById('search-field').value;
+//    serchMeals
+   console.log(searchText);
+}
+
+
+loadMeals();
